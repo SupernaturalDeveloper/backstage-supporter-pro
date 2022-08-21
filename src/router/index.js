58845d8584eb0c0bf83867c50e012d6c3bc47aae
神马玩意 <2210183204@qq.com>
2022-08-21 21:15:58 +0800

    '添加了路由'

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+const HomeView = () => import("@/views/HomeView/HomeView");
+const LoginView = () => import("@/views/LoginView/LoginView");
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    component: HomeView,
+    meta: {
+      login_required: true
+    }
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/login",
+    component: LoginView
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+router.beforeEach((to, from, next) => {
+  if (to.meta.login_required) {
+    if (window.localStorage.getItem("UserInfo")) {
+      next()
+    } else {
+      next("/login")
+    }
+  } else {
+    next();
+  }
 })
 
 export default router
---
+++ src/views/LoginView/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "LoginView",
+
+        data() {
+            return {};
+        },
+
+        async mounted() {
+            const params = {
+                params: {
+                    loginData: {
+                        name: "",
+                        password: "",
+                    },
+                },
+            };
+            let res = await this.$R.postLogin(params);
+            console.log(res);
+        },
+
+        methods: {},
+    };
+</script>
+
+<style lang="scss" scoped>
+</style>
